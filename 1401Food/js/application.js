21925c4203037948dcfa5450ab9707da04cc7d54
huynxtb <huynx.dev.dn@gmail.com>
2021-07-11 17:47:24 +0700

    fix bug

--- 1401Food/js/application.js
+++ 1401Food/js/application.js
@@ -3,13 +3,18 @@ $(function () {
     var thisTotal = JSON.parse(localStorage.getItem('Orders'));
     var total_price = 0;
 
-    thisTotal.forEach(e => {
-        total_price += (e.price * e.quantity);
-    });
+    if (thisTotal !== null) {
+        thisTotal.forEach(e => {
+            total_price += (e.price * e.quantity);
+        });
 
-    var money = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(total_price).toLocaleString();
-    $(".total-price").text(money.split('￥')[1] + '₫');
-    $(".total-item").text(thisTotal.length);
+        var money = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(total_price).toLocaleString();
+        $(".total-price").text(money.split('￥')[1] + '₫');
+        $(".total-item").text(thisTotal.length);
+    }else{
+        $(".total-price").text('0₫');
+        $(".total-item").text(0);
+    }
 
     _initEvents();
     _initShoppingCart();
@@ -44,8 +49,23 @@ function _initEvents() {
             total_price += (e.price * e.quantity);
         });
 
-        $(".total-price").text(total_price);
+        var money = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(total_price).toLocaleString();
+        $(".total-price").text(money.split('￥')[1] + '₫');
         $(".total-item").text(thisTotal.length);
+    });
+
+    $(".btn-payment").click(function (e) {
+        e.preventDefault();
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Đặt hàng thành công.',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        
+        localStorage.removeItem('Orders');
+        setTimeout(function(){ location.href = 'index.html'; }, 2000);
     });
 }
 
@@ -54,13 +74,14 @@ function _initShoppingCart() {
 
     var html = '';
 
-    currentOrders.forEach(e => {
+    if (currentOrders !== null) {
+        currentOrders.forEach(e => {
 
-        var money = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price).toLocaleString();
-        var money_convert = money.split('￥')[1] + '₫';
-        var total = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price * e.quantity).toLocaleString();
-        var total_convert = total.split('￥')[1] + '₫';
-        html += `<tr>
+            var money = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price).toLocaleString();
+            var money_convert = money.split('￥')[1] + '₫';
+            var total = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price * e.quantity).toLocaleString();
+            var total_convert = total.split('￥')[1] + '₫';
+            html += `<tr>
                                     <td class="shoping__cart__item">
                                         <img src="${e.image.replace('-', '/')}" style="width: 100px; height: 100px;" alt="">
                                         <h5>Khô gà lá chanh</h5>
@@ -83,23 +104,26 @@ function _initShoppingCart() {
                                     </td>
                                 </tr>`
 
-    });
-    
+        });
+    }
+
     $(".table-body").html(html)
 }
 
-function _initCheckout(){
-    
+function _initCheckout() {
+
     var currentOrders = JSON.parse(localStorage.getItem('Orders'));
 
     var html = '';
 
-    currentOrders.forEach(e => {
-        var total = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price * e.quantity).toLocaleString();
-        var total_convert = total.split('￥')[1] + '₫';
-        html += `<li>${e.name}<span>${total_convert}</span></li>`
+    if (currentOrders !== null) {
+        currentOrders.forEach(e => {
+            var total = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(e.price * e.quantity).toLocaleString();
+            var total_convert = total.split('￥')[1] + '₫';
+            html += `<li>${e.name}<span>${total_convert}</span></li>`
 
-    });
-    
+        });
+    }
+
     $(".ul-item").html(html)
 }
